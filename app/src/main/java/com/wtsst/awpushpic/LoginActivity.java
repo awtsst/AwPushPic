81a3b94d4440b165208cb8ff10eea7f118c0c343
sunshuntao <staryumou@163.com>
2016-04-11 10:57:04 +0800

    The is Project

--- app/src/main/java/com/wtsst/awpushpic/LoginActivity.java
+++ app/src/main/java/com/wtsst/awpushpic/LoginActivity.java
@@ -3,6 +3,7 @@ package com.wtsst.awpushpic;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -334,7 +335,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+//                finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
